f04b3d55a5252fba208d40cd70060ca2aa23e797
Tiger Abrodi <tigerabrodi@gmail.com>
2023-12-31 13:37:14 +0100

    report pair

--- README.md
+++ README.md
@@ -7,3 +7,8 @@ Very late lol.
 ## UTF 8 encoding
 
 When reading files with fs, you need to specify the encoding. Otherwise, you will get a buffer. You can convert the buffer to a string with `buffer.toString('utf-8')`. Or when reading the file, you can specify the encoding as a second argument.
+
+# Report Repair
+
+- Figure out if a number adds to number equals 2020 by subtracting 2020 from the number and checking if the result is in the Set.
+- Set a label to the outer loop to break from inner loop all the way to outer loop
--- report-repair/index.js
+++ report-repair/index.js
@@ -1,18 +1,30 @@
 const fs = require("fs");
 
-const input = fs.readFileSync("./input.txt", "utf-8").split("\n").map(Number);
+const arr = fs.readFileSync("./input.txt", "utf-8").split("\n").map(Number);
 
 let result = 0;
 
-for (let i = 0; i < input.length; i++) {
-  for (let j = i + 1; j < input.length; j++) {
-    for (let k = j + 1; k < input.length; k++) {
-      if (input[i] + input[j] + input[k] === 2020) {
-        result = input[i] * input[j] * input[k];
-        break;
-      }
-    }
-  }
-}
+const arrSet = new Set(arr);
+
+// part 1, efficient
+// for (const num of arr) {
+//   const diff = 2020 - num; // Sum would be a number minus the current number, because adding the current number to the sum would be 2020
+//   if (arrSet.has(diff)) {
+//     result = num * diff;
+//     break;
+//   }
+// }
+
+// Brute force part 2
+// outer: for (let i = 0; i < input.length; i++) {
+//   for (let j = i + 1; j < input.length; j++) {
+//     for (let k = j + 1; k < input.length; k++) {
+//       if (input[i] + input[j] + input[k] === 2020) {
+//         result = input[i] * input[j] * input[k];
+//         break outer;
+//       }
+//     }
+//   }
+// }
 
 console.log(result);
