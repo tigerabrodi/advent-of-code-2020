9f3c9d6eb07b90d1853a416db11154e84351151d
Tiger Abrodi <tigerabrodi@gmail.com>
2023-12-31 13:28:26 +0100

    report part 2

--- report-repair/index.js
+++ report-repair/index.js
@@ -6,9 +6,11 @@ let result = 0;
 
 for (let i = 0; i < input.length; i++) {
   for (let j = i + 1; j < input.length; j++) {
-    if (input[i] + input[j] === 2020) {
-      result = input[i] * input[j];
-      break;
+    for (let k = j + 1; k < input.length; k++) {
+      if (input[i] + input[j] + input[k] === 2020) {
+        result = input[i] * input[j] * input[k];
+        break;
+      }
     }
   }
 }
